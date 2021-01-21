3d3fc868ae089f20dea9e954a2405604d84d66b1
Marat <shaimardanov@infomatika.ru>
2021-01-21 21:11:59 +0300

    module8-task1: Добавляет метку на карту

--- frontend/views/tasks/view.php
+++ frontend/views/tasks/view.php
@@ -193,6 +193,9 @@ $user = \Yii::$app->user->identity;
                 center: [latitude, longitude],
                 zoom: 14
             });
+            myMap.geoObjects.add(new ymaps.Placemark([latitude, longitude], {}, {
+                preset: 'islands#redIcon',
+            }));
         }
     }
 </script>
