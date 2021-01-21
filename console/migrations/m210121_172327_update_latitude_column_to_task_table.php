4018d3fb3e4e29b92170536c3d6be2d5ba80a915
Marat <shaimardanov@infomatika.ru>
2021-01-21 21:11:15 +0300

    module8-task1: Правит тип полей latitude и longitude

---
+++ console/migrations/m210121_172327_update_latitude_column_to_task_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210121_172327_update_latitude_column_to_task_table
+ */
+class m210121_172327_update_latitude_column_to_task_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $sql = "ALTER TABLE task CHANGE `latitude` `latitude` DECIMAL(13,10)";
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210121_172327_update_latitude_column_to_task_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210121_172327_update_latitude_column_to_task_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
